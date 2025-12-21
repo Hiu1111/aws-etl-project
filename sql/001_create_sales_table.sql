5c0da48a5cd9fee619d3d95f98556f183579be63
Hiu Cheung <hiucheung@Hius-MacBook-Air.local>
2025-12-21 21:05:01 +0800

    Add create table SQL

--- sql/001_create_sales_table.sql
+++ sql/001_create_sales_table.sql
@@ -1,7 +1,6 @@
 CREATE TABLE IF NOT EXISTS sales (
-    order_id      INT,
-    customer_id   INT,
-    amount        NUMERIC(10,2),
-    tax           NUMERIC(10,2),
-    order_date    DATE
+  order_id     INT,
+  customer_id  INT,
+  amount       NUMERIC(10,2),
+  date         DATE
 );
